cd327a75cfc1df03b735deb2614ddac13640ba68
Guilherme Doval <doval.guilherme94@gmail.com>
2022-06-25 00:18:37 -0300

    ✨Feat: Create CategoriesRepository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,36 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoriesRepository };
