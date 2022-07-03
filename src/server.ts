8f52120ae2aa36db894ca397619d74998952b741
Guilherme Doval <doval.guilherme94@gmail.com>
2022-07-03 19:24:27 -0300

    ✨Feat: Add Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 const port = process.env.EXPRESS_PORT || 3333;
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use(router);
 
