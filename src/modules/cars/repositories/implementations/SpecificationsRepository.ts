c33e72d2359e091908f0267e74a2fd237fe61a8a
Guilherme Doval <doval.guilherme94@gmail.com>
2022-07-03 16:22:25 -0300

    🔧Fix: Refactor Specifications

---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,37 @@
+import { Specification } from "../../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "../ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO) {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string) {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+
+  list(): Specification[] {
+    return this.specifications;
+  }
+}
+
+export { SpecificationsRepository };
